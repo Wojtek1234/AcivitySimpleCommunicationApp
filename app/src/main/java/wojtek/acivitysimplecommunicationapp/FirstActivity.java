d4ed41c46f2c880a2b8faa55c63cb431534509b6
Wojtek1234 <wojciechmaciejewski1985@gmail.com>
2014-10-26 12:56:32 +0100

    repair some issue

--- app/src/main/java/wojtek/acivitysimplecommunicationapp/FirstActivity.java
+++ app/src/main/java/wojtek/acivitysimplecommunicationapp/FirstActivity.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 
 
 public class FirstActivity extends Activity {
+    public static final String message = "MESSAGE";
+    public static final String message2 = "MESSAGE2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,10 @@ public class FirstActivity extends Activity {
         setContentView(R.layout.activity_first);
 
 
-
-        TextView textView=(TextView)findViewById(R.id.textFirst);
+        TextView textView = (TextView) findViewById(R.id.textFirst);
         textView.setText(doOnExtraRecive(savedInstanceState));
 
-        Button button=(Button)findViewById(R.id.buttonFirst);
+        Button button = (Button) findViewById(R.id.buttonFirst);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,32 +36,45 @@ public class FirstActivity extends Activity {
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
 
-        TextView textView=(TextView)findViewById(R.id.textFirst);
+        TextView textView = (TextView) findViewById(R.id.textFirst);
         textView.setText(doOnExtraRecive(intent.getExtras()));
     }
 
-    private void openIntentWithText(){
-        EditText editText=(EditText)findViewById(R.id.editFirst);
-        Intent intent=new Intent(getApplicationContext(),SecondActiv.class);
-        intent.putExtra("message",editText.getText().toString());
-
+    private void openIntentWithText() {
+        EditText editText = (EditText) findViewById(R.id.editFirst);
+        EditText editText2 = (EditText) findViewById(R.id.edit2First);
+        Intent intent = new Intent(getApplicationContext(), SecondActiv.class);
+        intent.putExtra(message, editText.getText().toString());
+        intent.putExtra(message2, editText2.getText().toString());
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         getApplicationContext().startActivity(intent);
-        finish();
+
     }
 
 
-    private String doOnExtraRecive(Bundle savedInstanceState){
+    private String doOnExtraRecive(Bundle savedInstanceState) {
         String newString;
         if (savedInstanceState == null) {
-            Bundle extras= getIntent().getExtras();
-            if(extras == null) {
-              return   newString= "";
+            Bundle extras = getIntent().getExtras();
+            if (extras == null) {
+                newString = "";
             } else {
-                return  newString= extras.getString("message");
+                newString = extras.getString(message);
             }
         } else {
-            return newString= (String) savedInstanceState.getSerializable("message");
+            newString = (String) savedInstanceState.getSerializable(message);
+        }
+
+        String newString2;
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if (extras == null) {
+                return newString2 = newString + "";
+            } else {
+                return newString2 = newString + extras.getString(FirstActivity.message2);
+            }
+        } else {
+            return newString2 = newString + (String) savedInstanceState.getSerializable(FirstActivity.message2);
         }
     }
 
--- app/src/main/java/wojtek/acivitysimplecommunicationapp/SecondActiv.java
+++ app/src/main/java/wojtek/acivitysimplecommunicationapp/SecondActiv.java
@@ -17,9 +17,9 @@ public class SecondActiv extends Activity {
         setContentView(R.layout.activity_second);
 
 
-        TextView textView=(TextView)findViewById(R.id.textSecond);
+        TextView textView = (TextView) findViewById(R.id.textSecond);
         textView.setText(doOnExtraRecive(savedInstanceState));
-        Button button=(Button)findViewById(R.id.buttonSecond);
+        Button button = (Button) findViewById(R.id.buttonSecond);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,36 +28,50 @@ public class SecondActiv extends Activity {
         });
 
     }
+
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
 
-        TextView textView=(TextView)findViewById(R.id.textSecond);
+        TextView textView = (TextView) findViewById(R.id.textSecond);
         textView.setText(doOnExtraRecive(intent.getExtras()));
     }
-    private void openIntentWithText(){
-        EditText editText=(EditText)findViewById(R.id.editSecond);
-        Intent intent=new Intent(getApplicationContext(),FirstActivity.class);
-        intent.putExtra("message",editText.getText().toString());
 
+    private void openIntentWithText() {
+        EditText editText = (EditText) findViewById(R.id.editSecond);
+        EditText editText2 = (EditText) findViewById(R.id.edit2Second);
+        Intent intent = new Intent(getApplicationContext(), FirstActivity.class);
+        intent.putExtra(FirstActivity.message, editText.getText().toString());
+        intent.putExtra(FirstActivity.message2, editText2.getText().toString());
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
         getApplicationContext().startActivity(intent);
-        finish();
     }
 
 
-    private String doOnExtraRecive(Bundle savedInstanceState){
+    private String doOnExtraRecive(Bundle savedInstanceState) {
         String newString;
         if (savedInstanceState == null) {
-            Bundle extras= getIntent().getExtras();
-            if(extras == null) {
-                return   newString= "";
+            Bundle extras = getIntent().getExtras();
+            if (extras == null) {
+                newString = "";
             } else {
-                return  newString= extras.getString("message");
+                newString = extras.getString(FirstActivity.message);
             }
         } else {
-            return newString= (String) savedInstanceState.getSerializable("message");
+            newString = (String) savedInstanceState.getSerializable(FirstActivity.message);
+        }
+
+        String newString2;
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if (extras == null) {
+                return newString2 = newString + "";
+            } else {
+                return newString2 = newString + extras.getString(FirstActivity.message2);
+            }
+        } else {
+            return newString2 = newString + (String) savedInstanceState.getSerializable(FirstActivity.message2);
         }
     }
 }
